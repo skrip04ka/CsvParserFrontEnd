4717e608885fd7e7e3b1a67b38eccc9b50799c24
skrip04ka <fury.2489@gmail.com>
2023-07-11 00:57:09 +0300

    view previous files

--- src/app/services/meas/meas.service.ts
+++ src/app/services/meas/meas.service.ts
@@ -4,6 +4,7 @@ import {MeasList} from "../../models/meas-list";
 import {Observable} from "rxjs";
 import {MeasData} from "../../models/meas-data";
 import {MetaInf} from "../../models/meta-inf";
+import {FilesInfo} from "../../models/files-info";
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +15,16 @@ export class MeasService {
     this.usersUrl = 'http://localhost:9005';
   }
 
-  getMeasByNames(names: string[], start: number, end: number): Observable<HttpEvent<any>> {
+
+
+  getMeasByNames(names: string[], start: number, end: number, id : number): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
 
     formData.append('names', names.toString());
     formData.append('start', start.toString());
     formData.append('end', end.toString());
 
-    const req = new HttpRequest('POST', `${this.usersUrl}/data/get-data`, formData, {
+    const req = new HttpRequest('POST', this.usersUrl + "/data/get-data/" + id, formData, {
       reportProgress: true,
       responseType: 'json'
     });
@@ -29,15 +32,15 @@ export class MeasService {
     return this.http.request(req);
   }
 
-  public getMeasNames(): Observable<string[]> {
-    return this.http.get<string[]>(this.usersUrl + "/data/names");
+  public getMeasNames(id : number): Observable<string[]> {
+    return this.http.get<string[]>(this.usersUrl + "/data/names/" + id);
   }
 
-  public getMetaInf(): Observable<MetaInf> {
-    return this.http.get<MetaInf>(this.usersUrl + "/data/meta-inf");
+  public getMetaInf(id : number): Observable<MetaInf> {
+    return this.http.get<MetaInf>(this.usersUrl + "/data/meta-inf/" + id);
   }
 
-  public analise(phA: string, phB: string, phC: string, stock?: number): Observable<MeasData> {
+  public analise(phA: string, phB: string, phC: string, id : number, stock?: number): Observable<MeasData> {
 
     let queryParams = new HttpParams();
     queryParams = queryParams.append("phAName",phA);
@@ -48,7 +51,11 @@ export class MeasService {
       queryParams = queryParams.append("stock",stock);
     }
 
-    return this.http.get<MeasData>(this.usersUrl + "/data/analise", {params:queryParams});
+    return this.http.get<MeasData>(this.usersUrl + "/data/analise/" + id, {params:queryParams});
+  }
+
+  public getFilesInfo(): Observable<FilesInfo[]> {
+    return this.http.get<FilesInfo[]>(this.usersUrl + "/data/files/");
   }
 
 }
