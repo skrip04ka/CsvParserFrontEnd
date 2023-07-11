26cfa7909feba8e810f25a3da0943b4c321e7507
skrip04ka <fury.2489@gmail.com>
2023-07-12 02:56:15 +0300

    graphs migrate to tab, fix meta dto

--- src/app/models/meta-inf.ts
+++ src/app/models/meta-inf.ts
@@ -1,8 +1,7 @@
 export class MetaInf {
   n: number = 0;
   freq: number = 0;
-  file1Name: string = "";
-  file2Name: string = "";
+  name: string = "";
   digital: number = 0;
   analog: number = 0;
   timeStart: string = "";
